a0f15d40de084b07548af299af10b68522350b63
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2022-12-04 18:21:05 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,93 +1,100 @@
-// write your code here
 document.addEventListener('DOMContentLoaded', (e) => {
-const div = document.getElementById('ramen-menu')
-
-function createImage(noodles) {
-    let div2 = document.createElement('div')
-    let img = document.createElement('img')
-    img.setAttribute('src', noodles.image)
-    img.setAttribute('onclick', 'expandImg()')
-    div2.appendChild(img)
-    div.appendChild(div2)
+    const div = document.getElementById('ramen-menu')
+    let form = document.getElementById('new-ramen')
+    const img = document.querySelector('.detail-image')
+    let p = document.getElementById('comment-display')
+    let ratingDisplay = document.getElementById('rating-display')
+    let h2 = document.querySelector('h2')
+    let h3 = document.querySelector('.restaurant')
     
-}
-
-function appendImage(arr) {
-    let div2 = document.createElement('div')
-    let img = document.createElement('img')
-    img.setAttribute('src', arr.image)
-    img.setAttribute('onclick', 'expandImg()')
-    div2.appendChild(img)
-    div.appendChild(div2)
-
-}
-
-function getImages() {
-    return fetch('http://localhost:3000/ramens')
-    .then((response) => response.json())
-    .then((data) => {
-        return data
-    })
-}
-function displayData() {
-    getImages().then((images) => {
-        images.forEach((image) => {
-            createImage(image)
-        });
-    })
-}
-
-
-displayData()
-
-//Submit Method 
-let form = document.getElementById('new-ramen')
-const img = document.querySelector('.detail-image')
-let p = document.getElementById('comment-display')
-let rating = document.getElementById('rating-display')
-let h2 = document.querySelector('h2')
-let h3 = document.querySelector('.restaurant')
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let arr = getFormData()
-    p.innerHTML = arr.comment
-    rating.innerHTML = arr.rating
-    h2.innerHTML = arr.name
-    h3.innerHTML = arr.restaurant
-    
-    img.setAttribute('src', arr.image1)
-    appendImage(arr)
-})
-
-function expandImg() {
-    getImages().then((noodles)=> {
-        noodles.forEach((noodle) => {
-            img.setAttribute('src', noodles.image)
-            p.innerHTML= noodles.comment
-            rating.innerHTML = noodles.rating
-            h2.innerHTML = noodles.name
-            h3.innerHTML = noodles.restaurant
-            
+    function createImage(noodles) {
+        let div2 = document.createElement('div')
+        let img = document.createElement('img')
+        img.setAttribute('src', noodles.image)
+        img.addEventListener('click', (e) => {
+            renderImage(noodles)
         })
+        div2.appendChild(img)
+        div.appendChild(div2)
+        
+    }
+    function renderImage(noodles) {
+        img.src = noodles.image
+        h2.textContent = noodles.name
+        h3.textContent = noodles.restaurant
+        ratingDisplay.textContent = noodles.rating
+        p.textContent = noodles.comment
+
+
+    }
+    
+   
+    
+    function getImages() {
+        return fetch('http://localhost:3000/ramens')
+        .then((response) => response.json())
+        .then((data) => {
+            return data
+        })
+    }
+    function displayData() {
+        getImages().then((images) => {
+            images.forEach((image) => {
+                createImage(image)
+            });
+        })
+    }
+    
+    
+    displayData()
+    
+    //Submit Method 
+    function appendImage(arr) {
+        let div2 = document.createElement('div')
+        let img = document.createElement('img')
+        img.setAttribute('src', arr.image)
+        div2.appendChild(img)
+        div.appendChild(div2)
+    
+    }
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const {name,restaurant, image1, rating, comment} = getFormData()
+        p.innerHTML = comment
+        ratingDisplay.innerHTML = rating
+        h2.innerHTML = name
+        h3.innerHTML = restaurant
+        
+        img.setAttribute('src', image1)
+        addData(name, restaurant, image1, rating, comment)
+    })   
+    
+    function getFormData() {
+        let name = form.name.value
+        let restaurant = form.restaurant.value
+        let image1 = form.image.value
+        let rating = form.rating.value
+        let comment = form.paragraph.value
+        return {name, restaurant, image1, rating, comment}
+    
+    }
+
+    function addData(name,restaurant, image, rating, comment) {
+        fetch('http://localhost:3000/ramens', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({ name,restaurant, image, rating, comment})
+        })
+    }
+    
+        
+        
+    
+    
+    
+    
     })
-}
-
-
-function getFormData() {
-    let name = form.name.value
-    let restaurant = form.restaurant.value
-    let image1 = form.image.value
-    let rating = form.rating.value
-    let comment = form.paragraph.value
-    return {name, restaurant, image1, rating, comment}
-
-}
-
-    
-    
-
-
-
-
-})
